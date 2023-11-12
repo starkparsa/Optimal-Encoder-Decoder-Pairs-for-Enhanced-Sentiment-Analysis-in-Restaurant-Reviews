5fd69a3dfd674fe15c100cb7ec73e1ba2801961a
HABS8 <122919252+HABS8@users.noreply.github.com>
2023-11-11 22:06:08 -0500

    first commit
    first

--- extraction.py
+++ extraction.py
@@ -1,43 +1,17 @@
-import os
 import pandas as pd
+import re
 
-# Define the directories for positive and negative reviews
-positive_data_dir = r'C:\Users\Hasan Angel\CAP_5771_P\aclImdb\train\pos'
-negative_data_dir = r'C:\Users\Hasan Angel\CAP_5771_P\aclImdb\train\neg'
+# Load the dataset
+df = pd.read_csv('IMDB_Dataset.csv')
 
-# Function to extract text from a file
-def extract_text(file_path):
-    with open(file_path, 'r', encoding='utf-8') as file:
-        text = file.read()
-    return text
+# Mapping labels to numerical values
+df['label'] = df['label'].map({'positive': 1, 'negative': 0})
 
-# List to store data
-data = []
+# Remove <br /><br /> from reviews using regular expression
+df['review'] = df['review'].apply(lambda x: re.sub(r'<br /><br />', '', x))
 
-# Iterate through files in the negative reviews directory
-for filename in os.listdir(negative_data_dir):
-    if filename.endswith('.txt'):
-        file_path = os.path.join(negative_data_dir, filename)
-        text = extract_text(file_path)
-        label = 0  # Negative label
-        data.append({'text': text, 'label': label})
+# Save the processed data to a new CSV file
+df.to_csv('processed_IMDB_Dataset.csv', index=False)
 
-# Iterate through files in the positive reviews directory
-for filename in os.listdir(positive_data_dir):
-    if filename.endswith('.txt'):
-        file_path = os.path.join(positive_data_dir, filename)
-        text = extract_text(file_path)
-        label = 1  # Positive label
-        data.append({'text': text, 'label': label})
-
-# Create a pandas DataFrame from the data
-df = pd.DataFrame(data)
-
-# Shuffle the dataset
-df = df.sample(frac=1).reset_index(drop=True)
-
-# Save the shuffled dataset to a CSV file
-df.to_csv('shuffled_dataset.csv', index=False)
-
-# Display the first few rows of the shuffled dataset
+# Display the first few rows of the processed dataset
 print(df.head())
