02bb28a50a12f442808f0c35389bf348d4f6d9ba
HABS8 <122919252+HABS8@users.noreply.github.com>
2023-12-07 23:30:37 -0500

    done code

--- main.py
+++ main.py
@@ -5,77 +5,117 @@
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score
-from decoders.svm import SVMClassifier
-from sklearn.feature_extraction.text import TfidfTransformer
-import pickle
-import inspect
-import matplotlib.pyplot as plt
+from sklearn.svm import SVC
+from sklearn.metrics import classification_report
+import csv
+
+class SVMClassifier:
+    def __init__(self, input_dim=None):
+        self.model = SVC()
+        self.results = {'classification_report': None}
+
+    def train(self, X, y):
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+        self.model.fit(X_train, y_train)
+
+        # Record overall metrics
+        y_test_pred = self.model.predict(X_test)
+        test_report = classification_report(y_test, y_test_pred, output_dict=True)
+        self.results['classification_report'] = test_report
+
+    def predict(self, input_embedding):
+        # Make a prediction using the trained model
+        return self.model.predict([input_embedding])
+
+    def save_results(self, filename):
+        # Save the overall metrics as a CSV file
+        with open(filename, 'w', newline='') as file:
+            writer = csv.writer(file)
+            writer.writerow(["Class", "Precision", "Recall", "F1-Score", "Support"])
+            for class_label, metrics in self.results['classification_report'].items():
+                if class_label.isnumeric():  # Exclude non-numeric labels (e.g., 'accuracy')
+                    writer.writerow([class_label, metrics['precision'], metrics['recall'], metrics['f1-score'], metrics['support']])
+
+        print(f"Results saved to {filename}")
 
 def train_and_save_classifier(classifier, X, y, file_suffix):
-    if 'input_dim' in inspect.signature(classifier.__init__).parameters:
-        input_dim = X.shape[1]
-        model = classifier(input_dim=input_dim)
-    else:
-        model = classifier()
-
+    model = classifier()
     model.train(X, y)
-    model.save_results(f'results/yelp_{file_suffix}.pkl')
 
-    # Generate and save plots
-    accuracy_plot_path = f'results/yelp_{file_suffix}_accuracy.png'
+    # Save the results
+    filename = f'results/yelp_full_{file_suffix}.csv'
+    model.save_results(filename)
 
-    plt.figure(figsize=(12, 6))
+    # Display the classification report
+    print("Classification Report:")
+    print(model.results['classification_report'])
 
-    # Calculate accuracy directly using scikit-learn
-    predictions = model.predict(X)
-    accuracy = accuracy_score(y, predictions)
-
-    # Plot accuracy
-    plt.subplot(1, 2, 1)
-    plt.plot([accuracy], label='Accuracy')
-    plt.title('Accuracy Over Training')
-    plt.xlabel('Epochs')
-    plt.ylabel('Accuracy')
-    plt.legend()
-    plt.grid(True)
-
-    # Plot loss (commented out for SVM)
-    # plt.subplot(1, 2, 2)
-    # plt.plot(loss_values, label='Loss')
-    # plt.title('Loss Over Training')
-    # plt.xlabel('Epochs')
-    # plt.ylabel('Loss')
-    # plt.legend()
-    # plt.grid(True)
-
-    # Save plots
-    plt.tight_layout()
-    plt.savefig(accuracy_plot_path)
-    # plt.savefig(loss_plot_path)  # Commented out for SVM
-    plt.close()
+    return model  # Return the trained model
 
 def load_and_process_embeddings(file_path, column_name):
     df = pd.read_pickle(file_path)
-    df[column_name] = df[column_name].apply(lambda x: np.array(x))
+    df[column_name] = df[column_name].apply(np.array)
     X = np.array(df[column_name].tolist())
     y = df['labels'].values
 
     return X, y
 
 def main():
+    # load the model and get the metrics
+
     embeddings_list = [('BART', 'bart_embeddings')]
 
     classifiers = [
         (SVMClassifier, 'svm'),
     ]
 
+    trained_models = []
+
     for embedding_type, column_name in embeddings_list:
         file_path = f'embeddings/yelp_{embedding_type}_embeddings.pkl'
         X, y = load_and_process_embeddings(file_path, column_name)
 
         for classifier, suffix in classifiers:
-            train_and_save_classifier(classifier, X, y, f'{embedding_type}_{suffix}')
+            trained_model = train_and_save_classifier(classifier, X, y, f'{embedding_type}_{suffix}')
+            trained_models.append((trained_model, embedding_type))
+    
+    print("How the model will be used to make Predictions")
+
+    # review recommendation for restaurant with id 'M0c99tzIJPIbrY_RAO7KSQ'
+
+    file_path = 'embeddings/yelp_BART_embeddings.pkl'
+
+    # Read the pickle file into a DataFrame
+    df = pd.read_pickle(file_path)
+
+    # Specify the business ID you want to filter
+    target_business_id = 'M0c99tzIJPIbrY_RAO7KSQ'
+
+    # Create a new DataFrame with only rows that match the specified business ID
+    filtered_df = df[df['business_id'] == target_business_id]
+
+    # Display the new DataFrame
+    print(filtered_df.head())
+
+    # Calculate the average of the 'bart_embeddings' column
+    average_embedding = np.mean(np.stack(filtered_df['bart_embeddings']), axis=0)
+
+    print("Average Embedding:", average_embedding)
+
+    # Make predictions using the trained models
+    for trained_model, embedding_type in trained_models:
+        prediction = trained_model.predict(average_embedding)
+        print(f"Prediction using {embedding_type} model:", prediction)
+        if prediction[0] == 1:
+            print("Really don't go to this restaurant!")
+        elif prediction[0] == 2:
+            print("I would advise against it.")
+        elif prediction[0] == 3:
+            print("It's average.")
+        elif prediction[0] == 4:
+            print("I would advise for it.")
+        elif prediction[0] == 5:
+            print("Go to this restaurant!")
 
 if __name__ == "__main__":
     main()
